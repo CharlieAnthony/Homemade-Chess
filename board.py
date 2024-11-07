d300d2731c9d6b5b6c04152b86ab4f8fc1145145
Charlie Anthony <charliejohnanthony@gmail.com>
2024-11-07 18:55:50 +0000

    attempting to draw available moves (and failing!)

--- board.py
+++ board.py
@@ -11,11 +11,13 @@ class Board:
 						["", "", "", "", "", "", "", ""],
 						["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
 						["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]]
-
+		self.available_moves = [(3, 4), (4, 4), (3, 3), (4, 3)]
 		self.square_size = 40
 		self.screen = screen
 		self.x_offset, self.y_offset = 30, 30
-
+		self.is_white_turn = True
+		self.can_white_castle = True
+		self.can_black_castle = True
 		self.selected_piece = None
 
 		self.print_board()
@@ -35,6 +37,18 @@ class Board:
 					img = pygame.image.load(f"./assets/{val}.png")
 					img_resized = pygame.transform.scale(img, (self.square_size, self.square_size))
 					self.screen.blit(img_resized, (x_pos, y_pos))
+
+		if self.selected_piece:
+			for (x, y) in self.available_moves:
+				square_offset = self.square_size // 2
+				x_pos, y_pos = self.x_offset + square_offset + (x * self.square_size), self.y_offset + square_offset + (y * self.square_size)
+				# TODO: create dot image and display like a piece
+				# circle = pygame.draw.circle(
+				# 	surface=self.screen,
+				# 	color=(115, 115, 115),
+				# 	center=(x_pos, y_pos),
+				# 	radius=square_offset // 2.5,
+				# 	width=0)
 		pygame.display.flip()
 
 	def get_clicked_cell(self, pos):
@@ -51,11 +65,17 @@ class Board:
 			return []
 		self.selected_piece = (x, y)
 
+
 	def move_piece(self, new_pos):
 		x_old, y_old = self.selected_piece
 		x_new, y_new = new_pos
 		piece = self.state[y_old][x_old]
+		if (self.is_white_turn and piece[0] != 'w') or (not self.is_white_turn and piece[0] != 'b'):
+			print("not your turn!")
+			self.selected_piece = ""
+			return
 		self.state[y_old][x_old] = ""
 		self.state[y_new][x_new] = piece
 		self.print_board()
 		self.selected_piece = ""
+		self.is_white_turn = not self.is_white_turn
